9e1a3491fc71e19bae3f6054d68dccca84c08b4d
Gregory Boyce <gregory.boyce@gmail.com>
2016-04-09 13:50:48 +0000

    Setup file and start of dryer script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='pybrate',
+      version='0.1',
+      description='Python module for detecting movement',
+      author='Gregory Boyce',
+      author_email='gregory.boyce@gmail.com',
+      url='https://github.com/gdfuego/pybrate',
+      py_modules = ['pybrate'],
+      scripts=['dryer']
+      )
