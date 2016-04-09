8607b15c42a2ff149fa8ab01f8e5f358e4e8b21a
Gregory Boyce <gregory.boyce@gmail.com>
2016-04-09 20:19:08 +0000

    Completed Dryer object

--- dryer.py
+++ dryer.py
@@ -4,8 +4,8 @@ from pybrate import VibrateSensor
 from datetime import datetime
 
 class Dryer(VibrateSensor):
+    """Object for tracking the status of a dryer"""
     def __init__(self):
-        from datetime import datetime
         super(Dryer, self).__init__()
         self.running = False
         self.refresh = False
@@ -13,8 +13,20 @@ class Dryer(VibrateSensor):
         self.movement_ended = datetime(2000, 1, 1)
 
     def update(self):
+        """Update object states"""
         motion = self.motion_detected()
         if motion:
-            runningtime = datetime.now() - self.movement_started
-            if (not self.running and runningtime > 60):
-                self.running = True
+            if not self.running:
+                running_time = datetime.now() - self.movement_started
+                if running_time.seconds > 60:
+                    self.running = True
+            else:
+                self.movement_ended = datetime.now()
+        else:
+            if self.running:
+                stopped_time = datetime.now() - self.movement_ended
+                if stopped_time.seconds > 60:
+                    self.movement_ended = datetime.now()
+                    self.running = False
+            else:
+                self.movement_started = datetime.now()
