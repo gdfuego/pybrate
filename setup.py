0aba8a56a60cb4f416a0eac5049ebec6a8c69e85
Gregory Boyce <gregory.boyce@gmail.com>
2016-04-10 12:14:02 +0000

    Have track_dryer send notifications to IFTTT

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pybrate',
-      version='0.1',
+      version='0.2',
       description='Python module for detecting movement',
       author='Gregory Boyce',
       author_email='gregory.boyce@gmail.com',
