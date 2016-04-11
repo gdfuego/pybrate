772eacbb9b21d76b75c3e2fe16eb2ff844e52981
Gregory Boyce <gregory.boyce@gmail.com>
2016-04-11 02:42:12 +0000

    Always have "update" return True

--- pybrate.py
+++ pybrate.py
@@ -36,6 +36,7 @@ class Dryer(VibrateSensor):
         super(Dryer, self).__init__()
         self.running = False
         self.refresh = False
+        self.changed = False
         self.movement_started = datetime.now()
         self.movement_ended = datetime.now()
 
@@ -43,24 +44,24 @@ class Dryer(VibrateSensor):
         """Attempts to detect motion by running up to 5 polling events.
         A single motion event out of 5 will count as motion.
         Returns True when the status changed, or False if it remains the same"""
-        changed = False
+        self.changed = False
         for _ in range(5):
             if self.motion_detected():
                 if not self.running:
                     running_time = datetime.now() - self.movement_started
                     if running_time.seconds > 60:
                         self.running = True
-                        changed = True
+                        self.changed = True
                 else:
                     self.movement_ended = datetime.now()
-                return changed
+                return True
         # We returned no motion 5 times in a row
         if self.running:
             stopped_time = datetime.now() - self.movement_ended
             if stopped_time.seconds > 60:
                 self.movement_ended = datetime.now()
                 self.running = False
-                changed = True
+                self.changed = True
         else:
             self.movement_started = datetime.now()
-        return changed
+        return True
